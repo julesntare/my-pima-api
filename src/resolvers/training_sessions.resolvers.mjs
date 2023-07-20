7290fe2db537dde0a0984de45f828d8df614bea9
julesntare <julesntare@gmail.com>
2023-07-20 17:19:54 +0200

    added loading training sessions per project

--- src/resolvers/training_sessions.resolvers.mjs
+++ src/resolvers/training_sessions.resolvers.mjs
@@ -1,3 +1,5 @@
+import Projects from "../models/projects.models.mjs";
+
 const TrainingSessionsResolvers = {
   Query: {
     trainingSessionStatistics: async (_, { ts_id }, { sf_conn }) => {
@@ -37,6 +39,58 @@ const TrainingSessionsResolvers = {
                 ts_name: training_session.Name,
                 ts_module: training_session.Module_Name__c,
                 ts_group: training_session.Training_Group__c,
+                ts_status: training_session.Session_Status__c,
+                total_male: training_session.Male_Attendance__c || 0,
+                total_female: training_session.Female_Attendance__c || 0,
+              };
+            }
+          ),
+        };
+      } catch (err) {
+        console.log(err);
+
+        return {
+          message: err.message,
+          status: err.status,
+        };
+      }
+    },
+
+    trainingSessionsByProject: async (_, { proj_name }, { sf_conn }) => {
+      try {
+        const project = await Projects.findOne({
+          where: { project_name: proj_name },
+        });
+
+        if (!project) {
+          return {
+            message: "Project not found",
+            status: 404,
+          };
+        }
+
+        // get training sessions
+        const training_sessions = await sf_conn.query(
+          `SELECT Id, Name, Module_Name__c, Training_Group__c, Training_Group__r.TNS_Id__c, Session_Status__c, Male_Attendance__c, Female_Attendance__c, Trainer__c, Project_Name__c FROM Training_Session__c WHERE Training_Group__r.Group_Status__c='Active' AND Project_Name__c = '${proj_name}'`
+        );
+
+        // check if training sessions exist
+        if (training_sessions.totalSize === 0) {
+          return {
+            message: "Training sessions not found",
+            status: 404,
+          };
+        }
+
+        return {
+          message: "Training sessions fetched successfully",
+          status: 200,
+          trainingSessions: training_sessions.records.map(
+            (training_session) => {
+              return {
+                ts_id: training_session.Id,
+                ts_name: training_session.Name,
+                ts_module: training_session.Module_Name__c,
                 ts_status: training_session.Session_Status__c,
                 total_male: training_session.Male_Attendance__c || 0,
                 total_female: training_session.Female_Attendance__c || 0,
--- src/typeDefs/training_sessions.typeDefs.mjs
+++ src/typeDefs/training_sessions.typeDefs.mjs
@@ -1,46 +1,47 @@
 import { gql } from "apollo-server-express";
 
 const TrainingSessionsTypeDefs = gql`
-    type TrainingSession {
-        ts_id: ID!
-        ts_name: String!
-        ts_module: String!
-        ts_group: String
-        ts_status: String!
-        total_males: Int
-        total_females: Int
-    }
+  type TrainingSession {
+    ts_id: ID!
+    ts_name: String!
+    ts_module: String!
+    ts_group: String
+    ts_status: String!
+    total_males: Int
+    total_females: Int
+  }
 
-    type Query {
-        trainingSessions: AllTrainingSessionsResponse
-        trainingSession(ts_id: ID!): TrainingSessionResponse
-        trainingSessionsByGroup(tg_id: String!): AllTrainingSessionsResponse
-        trainingSessionStatistics(ts_id: ID!): TrainingSessionStatisticsResponse
-    }
+  type Query {
+    trainingSessions: AllTrainingSessionsResponse
+    trainingSession(ts_id: ID!): TrainingSessionResponse
+    trainingSessionsByProject(proj_name: String!): AllTrainingSessionsResponse
+    trainingSessionsByGroup(tg_id: String!): AllTrainingSessionsResponse
+    trainingSessionStatistics(ts_id: ID!): TrainingSessionStatisticsResponse
+  }
 
-    type AllTrainingSessionsResponse {
-        message: String!
-        status: Int!
-        trainingSessions: [TrainingSession]
-    }
+  type AllTrainingSessionsResponse {
+    message: String!
+    status: Int!
+    trainingSessions: [TrainingSession]
+  }
 
-    type TrainingSessionResponse {
-        message: String!
-        status: Int!
-        trainingSession: TrainingSession
-    }
+  type TrainingSessionResponse {
+    message: String!
+    status: Int!
+    trainingSession: TrainingSession
+  }
 
-    type TrainingSessionStatisticsResponse {
-        message: String!
-        status: Int!
-        statistics: TrainingSessionStatistics
-    }
+  type TrainingSessionStatisticsResponse {
+    message: String!
+    status: Int!
+    statistics: TrainingSessionStatistics
+  }
 
-    type TrainingSessionStatistics {
-        male_attendance: Int
-        female_attendance: Int
-        total_attendance: Int
-    }
+  type TrainingSessionStatistics {
+    male_attendance: Int
+    female_attendance: Int
+    total_attendance: Int
+  }
 `;
 
 export default TrainingSessionsTypeDefs;
