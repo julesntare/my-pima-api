5e23a635c16087015009a890051c64e4e460d316
julesntare <julesntare@gmail.com>
2023-08-01 09:36:29 +0200

    replaced fetch api with axios to fit in specific node version

--- src/utils/commCareApi.mjs
+++ src/utils/commCareApi.mjs
@@ -1,21 +1,21 @@
+import axios from "axios";
+
 const fetchImage = async (url) => {
   try {
     // fetch from url with authorization
-    const response = await fetch(url, {
-      method: "GET",
+    const response = await axios.get(url, {
       headers: {
-        authorization: `ApiKey ${process.env.COMMCARE_API_KEY}`,
+        Authorization: `ApiKey ${process.env.COMMCARE_API_KEY}`,
       },
     });
-    const resText = await response.text();
+
+    const resText = await response.data;
     // encode the response to base64
     const base64encodedData = base64encode(resText);
 
     // Send the base64 data to the frontend
     return base64encodedData;
   } catch (error) {
-    console.log(error);
-    // Handle any errors that might occur during the API call
     return null;
   }
 };
