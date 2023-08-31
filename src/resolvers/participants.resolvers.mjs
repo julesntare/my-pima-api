40d7a8007aba16586dc4b66be93358851736af49
julesntare <julesntare@gmail.com>
2023-08-31 14:08:11 +0200

    added participants upload

--- src/resolvers/participants.resolvers.mjs
+++ src/resolvers/participants.resolvers.mjs
@@ -238,27 +238,168 @@ const ParticipantsResolvers = {
 
         let { ext } = parse(filename);
 
-        // check if uploads folder exists
-        const uploadsFolder = join(
-          getDirName(import.meta.url),
-          "../../uploads"
-        );
+        // read file data
+        const chunks = [];
+        stream.on("data", (chunk) => {
+          chunks.push(chunk);
+        });
 
-        if (!fs.existsSync(uploadsFolder)) {
-          fs.mkdirSync(uploadsFolder);
-        }
+        stream.on("end", async () => {
+          const fileData = Buffer.concat(chunks);
+
+          const rows = fileData.toString().split("\n");
+
+          const header = rows[0].split(",");
+
+          // replace header values with Salesforce API names
+          header.forEach((value, index) => {
+            if (value === "HouseHold Name") {
+              header[index] = "Name";
+            } else if (value === "HouseHold Number") {
+              header[index] = "Household_Number__c";
+            } else if (value === "Last Name") {
+              header[index] = "Last_Name__c";
+            } else if (value === "Primary Household Member") {
+              header[index] = "Primary_Household_Member__c";
+            } else if (value === "TNS Id") {
+              header[index] = "TNS_Id__c";
+            } else if (value === "Gender") {
+              header[index] = "Gender__c";
+            } else if (value === "Age") {
+              header[index] = "Age__c";
+            } else if (value === "Phone Number") {
+              header[index] = "Phone_Number__c";
+            } else if (value === "Farm Size") {
+              header[index] = "Farm_Size__c";
+            } else if (value === "Training Group") {
+              header[index] = "Training_Group__c";
+            } else if (value === "Project") {
+              header[index] = "Project__c";
+            } else if (value === "Resend to OpenFN") {
+              header[index] = "Resend_to_OpenFN__c";
+            } else if (value === "Check Status") {
+              header[index] = "Check_Status__c";
+            } else if (value === "Create in Commcare") {
+              header[index] = "Create_In_CommCare__c";
+            }
+          });
+
+          // Get the indexes of the required columns
+          const requiredColumns = ["Farm_Size__c", "Training_Group__c"];
+          const nameColumnIndex = header.lastIndexOf("Name");
+          const columnIndexMap = requiredColumns.reduce((map, column) => {
+            map[column] = header.indexOf(column);
+            return map;
+          }, {});
+
+          // Process each row of data
+          const formattedData = rows.slice(1).map((row) => {
+            const values = row.split(",");
+            const formattedRow = {};
+
+            for (const column of requiredColumns) {
+              const index = columnIndexMap[column];
+              formattedRow[column] = values[index];
+            }
+
+            formattedRow["Name"] = values[nameColumnIndex];
+
+            return formattedRow;
+          });
+
+          const houseHoldRes = await sf_conn
+            .sobject("Household__c")
+            .create(formattedData, { allOrNone: true }, function (err, ret) {
+              return {
+                err,
+                ret,
+              };
+            });
+
+          console.log(houseHoldRes);
+
+          if (houseHoldRes.length > 0) {
+            // map data and headers for Participant__c
+            const participantsHeaders = [
+              "Name",
+              "Last_Name__c",
+              "Gender__c",
+              "Age__c",
+              "Phone_Number__c",
+              "Primary_Household_Member__c",
+              "TNS_Id__c",
+              "Training_Group__c",
+              "Resend_to_OpenFN__c",
+              "Check_Status__c",
+              "Create_In_CommCare__c",
+              "Household__c",
+            ];
+
+            const columnIndexMap = participantsHeaders.reduce((map, column) => {
+              map[column] = header.indexOf(column);
+              return map;
+            }, {});
+
+            const formattedPartsData = rows.slice(1).map((row) => {
+              const values = row.split(",");
+              const formattedRow = {};
+
+              for (const column of participantsHeaders) {
+                const index = columnIndexMap[column];
+                formattedRow[column] = values[index];
+              }
+
+              return formattedRow;
+            });
+
+            // insert res.id to Household__c field in participantsData
+            const participantsData = formattedPartsData.map((part, index) => {
+              return {
+                ...part,
+                Household__c: houseHoldRes[index].id,
+                Resend_to_OpenFN__c:
+                  part.Resend_to_OpenFN__c === "TRUE" ? true : false,
+                Create_In_CommCare__c: false,
+              };
+            });
+
+            console.log(participantsData);
+
+            const participantsRes = await sf_conn
+              .sobject("Participant__c")
+              .create(
+                participantsData,
+                { allOrNone: true },
+                function (err, ret) {
+                  return { err, ret };
+                }
+              );
+
+            console.log(participantsRes);
+          }
+        });
+
+        // check if uploads folder exists
+        // const uploadsFolder = join(
+        //   getDirName(import.meta.url),
+        //   "../../uploads"
+        // );
+
+        // if (!fs.existsSync(uploadsFolder)) {
+        //   fs.mkdirSync(uploadsFolder);
+        // }
 
         // name file with user_id and date
-        const newFilename = `participants-${Date.now()}${ext}`;
+        // const newFilename = `participants-${Date.now()}${ext}`;
 
-        let serverFile = join(
-          getDirName(import.meta.url),
-          `../../uploads/${newFilename}`
-        );
+        // let serverFile = join(
+        //   getDirName(import.meta.url),
+        //   `../../uploads/${newFilename}`
+        // );
 
-        let writeStream = createWriteStream(serverFile);
+        // let writeStream = createWriteStream(serverFile);
 
-        await stream.pipe(writeStream);
+        // await stream.pipe(writeStream);
 
         return {
           message: "New Participants uploaded successfully",
